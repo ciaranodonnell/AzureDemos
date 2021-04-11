e344be18f1e68b6e24d6929d53b9c57d2f677431
Ciaran ODonnell <wannabedeveloper@live.com>
2021-04-11 00:18:13 -0400

    Added the send receive loop

--- AzureDemos/ServiceBus/AzureDemos.SimpleServiceBus/Program.cs
+++ AzureDemos/ServiceBus/AzureDemos.SimpleServiceBus/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.ServiceBus;
@@ -13,30 +14,60 @@ namespace AzureDemos.SimpleServiceBus
 		{
 			Console.WriteLine("Starting our Service Bus Demo!");
 
-			
 			var queuePath = "ourqueue";
-			string namespaceConnectionString = string.Empty;
-			
-			ServiceBusConnection connection = new ServiceBusConnection(namespaceConnectionString);
+			var namespaceConnectionString =
+				"Endpoint=sb://ciaransyoutubedemos.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=plVg4/vEV4mlBWn1i5WU1FatmtxeHvbAOLqRIIHU8c0=";
+
+
+			await BasicReceiveMessage(namespaceConnectionString, queuePath);
+
+
+			while (true)
+			{
+				await BasicSendMessage(namespaceConnectionString, queuePath);
+				Console.ReadLine();
+			}
+		}
+
+		private static Task BasicReceiveMessage(string namespaceConnectionString, string queuePath)
+		{
+			QueueClient ourQueue =
+				new QueueClient(namespaceConnectionString, queuePath, ReceiveMode.ReceiveAndDelete, RetryPolicy.Default);
+
+			ourQueue.RegisterMessageHandler((message, cancelToken) =>
+			{
+				Console.WriteLine(Encoding.UTF8.GetString(message.Body));
+				return Task.CompletedTask;
+			}, (exceptionEventArgs) =>
+			{
+				return Console.Error.WriteLineAsync(exceptionEventArgs.Exception.ToString());
+			});
+			return Task.CompletedTask;
+		}
+
+
+		private static int messagenumber = 0;
+		private static async Task BasicSendMessage(string namespaceConnectionString, string queuePath)
+		{
 			
 			ManagementClient mgtClient = new ManagementClient(namespaceConnectionString);
-			
+
 			if ((await mgtClient.QueueExistsAsync(queuePath)) == false)
 			{
 				var queueDescription = new QueueDescription(queuePath);
 				queueDescription.MaxDeliveryCount = 10;
 				queueDescription.DefaultMessageTimeToLive = TimeSpan.MaxValue;
 				queueDescription.EnableDeadLetteringOnMessageExpiration = false;
-				
-				await  mgtClient.CreateQueueAsync(queueDescription);
+
+				await mgtClient.CreateQueueAsync(queueDescription);
 			}
 
 			QueueClient ourQueue =
-				new QueueClient(connection, queuePath, ReceiveMode.ReceiveAndDelete, RetryPolicy.Default);
+				new QueueClient(namespaceConnectionString, queuePath, ReceiveMode.ReceiveAndDelete, RetryPolicy.Default);
 
-			var messageBytes = Encoding.UTF8.GetBytes("This is our first message");
+			var messageBytes = Encoding.UTF8.GetBytes($"This is message {messagenumber}");
+			messagenumber++;
 			await ourQueue.SendAsync(new Message(messageBytes));
-			
 		}
 	}
 }
