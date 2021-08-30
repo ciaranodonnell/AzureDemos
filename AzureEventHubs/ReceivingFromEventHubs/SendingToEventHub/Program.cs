5bb418a97f8c34f26f0325d66ff07ab04747f754
Ciaran ODonnell <wannabedeveloper@live.com>
2021-08-29 21:44:09 -0400

    working demo

--- AzureEventHubs/ReceivingFromEventHubs/SendingToEventHub/Program.cs
+++ AzureEventHubs/ReceivingFromEventHubs/SendingToEventHub/Program.cs
@@ -8,37 +8,22 @@ namespace SendingToEventHub
 {
     class Program
     {
+        static int messageNumber = 0;
+
         static async Task Main(string[] args)
         {
             Console.WriteLine("Starting our Event Hub Producer");
 
             string namespaceConnectionString = "Endpoint=sb://eventhubyoutubedemos.servicebus.windows.net/;SharedAccessKeyName=sendpolicy;SharedAccessKey=0GeKILsXQNlvXmxA0uZ1j5kkRAdMAsqPEcD6qB0asZ8=";
             string eventHubName = "demoeventhub";
-            //            var ehClient = new Azure.Messaging.EventHubs.EventHubConnection(namespaceConnectionString);
-            await SendAFewMessages(namespaceConnectionString, eventHubName);
 
-            await SendAFewMessages(namespaceConnectionString, eventHubName);
-
-
-        }
-
-
-        public static async Task SendABatchOfMessages(string namespaceConnectionString, string eventHubName)
-        {
-
-            EventHubProducerClient client = new EventHubProducerClient(namespaceConnectionString, eventHubName);
-
-            var batch = await client.CreateBatchAsync(new CreateBatchOptions { PartitionKey = "world" });
             
-            for (int x = 0; x < 10; x++)
-            {
-                var msg = new Azure.Messaging.EventHubs.EventData("This is an event message");
-                batch.TryAdd(msg);
-            }
-
-            await client.SendAsync(batch);
-
+            await SendAFewMessages(namespaceConnectionString, eventHubName);
+            Console.WriteLine("Messages Sent");
+            Console.ReadLine();
         }
+
+
 
         public static async Task SendAFewMessages(string namespaceConnectionString, string eventHubName)
         {
@@ -47,16 +32,17 @@ namespace SendingToEventHub
 
             //var batch = await client.CreateBatchAsync(new CreateBatchOptions { PartitionKey = "hello" });
             List<EventData> events = new();
-
+            EventData[] messages = new EventData[1];
             for (int x = 0; x < 10; x++)
             {
-                var msg = new Azure.Messaging.EventHubs.EventData("This is an event message");
-                events.Add(msg);
+                var greetingPart = (x % 2 == 0 ? "Hello " : "World! ");
+                var msgText = $"{ messageNumber++} - {greetingPart}";
+                var msg = new Azure.Messaging.EventHubs.EventData(msgText);
+                messages[0] = msg;
+                await client.SendAsync(messages, new SendEventOptions { PartitionId = (x%2).ToString()});
+
             }
 
-            //batch.TryAdd(message);
-
-            await client.SendAsync(events, new SendEventOptions { PartitionKey = "hello" });
 
         }
     }
